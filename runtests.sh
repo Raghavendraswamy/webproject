c355c8a67dc635ca6818e5739cd3a5b3374d9880
RAGHAVENDRA SWAMY <32408841+Raghavendraswamy@users.noreply.github.com>
2024-09-24 05:37:44 +0530

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# Navigate to the directory containing the tests (if necessary)
-cd /path/to/your/test/directory
-
-# Run your tests
-# For example, using a testing framework like pytest, JUnit, etc.
-# Here are examples for different languages:
-
-# For Python with pytest
-# pytest
-
-# For Java with Maven
-# mvn test
-
-# For Node.js with Jest
-# npm test
-
-# For Ruby with RSpec
-# rspec
-
-# Add any additional test commands here
-
-# Capture the exit code
 
 #!/bin/bash
 
@@ -42,5 +20,4 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Call the function with the first parameter
 
